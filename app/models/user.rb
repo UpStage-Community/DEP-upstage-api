3408f93584135321c1f7b03cc22dbac9bf48f7da
Majd Murad <murad.majd@gmail.com>
2016-10-30 22:45:33 -0700

    Added user auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,13 @@ class User < ApplicationRecord
         default_url: "https://s-media-cache-ak0.pinimg.com/564x/6c/28/c1/6c28c1644b26303b67cd2879355e8d0a.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-    
+    validates :auth_token, uniqueness: true
+
+    before_create :generate_authentication_token!
+
+    def generate_authentication_token!
+        begin
+            self.auth_token = Devise.friendly_token
+        end while self.class.exists?(auth_token: auth_token)
+    end
 end
